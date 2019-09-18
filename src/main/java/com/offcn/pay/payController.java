9f3799744e53eea989317ff6e9ddda480cabe6e0
sunny <test@qq.com>
2019-09-18 16:04:32 +0800

    分支：test001
    新增支付模块
    （1）、payController
     (2)、payService

---
+++ src/main/java/com/offcn/pay/payController.java
@@ -0,0 +1,14 @@
+package com.offcn.pay;
+
+public class payController {
+
+    public void add(){
+
+        System.out.println("新增方法");
+    }
+
+
+    public void delete(){
+        System.out.println("删除方法");
+    }
+}
---
+++ src/main/java/com/offcn/pay/service/payService.java
@@ -0,0 +1,8 @@
+package com.offcn.pay.service;
+
+public interface payService {
+
+    public void  test111();
+
+    public void update();
+}
