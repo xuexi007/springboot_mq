71f5cedf9825d8d80410d12a994193d246ecfe00
sunny <test@qq.com>
2019-09-18 15:06:49 +0800

    修改主启动类
    1、新增注释
    2、新增方法test

--- src/main/java/com/offcn/SpringbootMqApplication.java
+++ src/main/java/com/offcn/SpringbootMqApplication.java
@@ -3,11 +3,20 @@ package com.offcn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 新增中文注释
+ */
 @SpringBootApplication
 public class SpringbootMqApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMqApplication.class, args);
+    }
+
+
+    public void test(){
+
+        System.out.println("test你哈");
     }
 
 }
